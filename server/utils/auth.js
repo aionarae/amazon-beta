e008914d03fc832a303822ff7e07022bdbc8f428
U6028449 <160793922+U6028449@users.noreply.github.com>
2024-08-01 20:11:24 -0600

    added schemas dir and utils dir to server

---
+++ server/utils/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'your_secret_key';
+const expiration = '2h';
+
+module.exports = {
+    authMiddleware: function ({ req }) {
+        let token = req.headers.authorization || '';
+
+        if (token) {
+            token = token.split(' ').pop().trim();
+            try {
+                const { data } = jwt.verify(token, secret, { maxAge: expiration });
+                req.user = data;
+            } catch {
+                console.log('Invalid token');
+            }
+        }
+
+        return req;
+    },
+    signToken: function ({ username, email, _id }) {
+        const payload = { username, email, _id };
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+};
