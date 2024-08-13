11efdbefc322e6a01a8ea11a2132bf9d97648509
Aiona <aionarae@users.noreply.github.com>
2024-08-12 20:03:15 -0600

    logging MONGODB_URI to verify its value

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/amazon-beta');
+console.log('Connecting to MongoDB with URI:', mongoUri);
 
 module.exports = mongoose.connection;
