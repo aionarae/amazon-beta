4c0d7bd6db16d1ed5c11c557c01ef14cfed611ff
Aiona <aionarae@users.noreply.github.com>
2024-08-12 20:35:49 -0600

    defined mongoUri (facepalm)

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/amazon-beta');
+const mongoUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/amazon-beta';
 console.log('Connecting to MongoDB with URI:', mongoUri);
+
+mongoose.connect(mongoUri);
 
 module.exports = mongoose.connection;
