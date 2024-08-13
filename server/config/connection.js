79afc92d5f0358b7d225b12723e9c2cc65e17ea1
Aiona <aionarae@users.noreply.github.com>
2024-08-12 20:51:06 -0600

    removing console.log because mongodb connection works now and I don't want to show my password

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const mongoUri = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/amazon-beta';
-console.log('Connecting to MongoDB with URI:', mongoUri);
 
 mongoose.connect(mongoUri);
 
